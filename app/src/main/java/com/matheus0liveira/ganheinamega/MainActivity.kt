a2ebc7b503bf0da066e89e93db7c16b21f4391bc
Matheus0liveiras <matheusfilho98.ms@gmail.com>
2023-12-02 17:33:45 -0300

    clean code

--- app/src/main/java/com/matheus0liveira/ganheinamega/MainActivity.kt
+++ app/src/main/java/com/matheus0liveira/ganheinamega/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.matheus0liveira.ganheinamega
 
+import android.content.Context
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -8,14 +10,25 @@ import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var prefs: SharedPreferences
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        prefs = getSharedPreferences("db", Context.MODE_PRIVATE)
+        val result = prefs.getString("result", null)
+
         setContentView(R.layout.activity_main)
 
         val editText = findViewById<EditText>(R.id.edit_number)
         val txtResult = findViewById<TextView>(R.id.text_result)
         val btnGenerate = findViewById<TextView>(R.id.btn_generate)
 
+        result?.let {
+
+            txtResult.text = it
+        }
 
         btnGenerate.setOnClickListener {
             onClickNumberGenerator(editText.text.toString(), txtResult)
@@ -32,6 +45,13 @@ class MainActivity : AppCompatActivity() {
         val numbers = generateRandomNumbers(qtd)
 
         textResult.text = numbers.joinToString(" - ")
+
+        prefs.edit().apply {
+            putString("result", textResult.text.toString())
+            apply() // Async and not return status
+            // editor.commit() // Sync and return status(boolean)
+        }
+
 
     }
 
